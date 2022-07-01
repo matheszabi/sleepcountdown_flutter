4dde5e70eb8261a4d42376fc7d12422a2cf2906e
Szabolcs Mathe <matheszabi@users.noreply.github.com>
2022-07-02 02:42:24 +0300

    added Widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,26 +5,64 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:sleepcountdown_flutter/main.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:sleepcountdown_flutter/MyHomePage.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+  testWidgets('Testing my widget', (WidgetTester tester) async {
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    final f0 = find.byType(MyHomePage);
+    expect(f0, findsOneWidget);
+    //get the UI value
+    MyHomePage myHomePage = tester.firstWidget(f0);
+    expect(myHomePage.title, 'Count down Flutter');
+    // get the model value:
+    MyHomePageState homePageState0 = tester.state(f0) as MyHomePageState;
+    expect(homePageState0.daysToSleep, 0);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    final f1 = find.textContaining("The selected date value:"); //label
+    expect(f1, findsOneWidget);
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    final f2 = find.textContaining("Days needed to sleep:"); //label
+    expect(f2, findsOneWidget);
+
+    final f3 = find.text('Select a Day "to"'); // Button
+    expect(f3, findsOneWidget);
+
+    // ensure not found:
+    final f4Not = find.byType(CupertinoDatePicker);
+    expect(f4Not, findsNothing);
+
+    // Tap the button
+    await tester.tap(f3);
+    // wait for showCupertinoModalPopup()
+    await tester.pumpAndSettle();
+
+    final f4 = find.byType(CupertinoDatePicker);
+    expect(f4, findsOneWidget);
+
+    CupertinoDatePicker cupertinoDatePicker = tester.firstWidget(f4);
+    DateTime initialDateTime = cupertinoDatePicker.initialDateTime;
+
+    int curYear = initialDateTime.year;
+    final f5 = find.text(curYear.toString()).last;
+    expect(f5, findsOneWidget);
+
+    await tester.dragFrom(tester.getCenter(f5), const Offset(0.0, -50.0));
+
+    // wait for all
+    await tester.pumpAndSettle();
+
+    // get reference for check:
+    MyHomePageState homePageState1year = tester.state(f0) as MyHomePageState;
+    int selYear = homePageState1year.selectedDate.year;
+
+    expect(curYear < selYear, true);
+
+    expect(homePageState1year.daysToSleep >= 365, true);
   });
 }
